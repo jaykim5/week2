5b99c1b04ef9d8ac87a64d1f7e577f0eb2c529fc
SoojongKim <79804328+jaykim5@users.noreply.github.com>
2021-05-14 12:29:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+//Dark-mode
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
